8fe1073ff62d7304d6698407e67d382898dc558d
deepak rana <deepakrana@ieee.org>
2021-08-18 14:06:04 +0530

    added music adn a lot of stuff

--- main.js
+++ main.js
@@ -64,3 +64,6 @@ sr.reveal('#chat7', {
       viewFactor: 0.2,
       rotate: {x: 0, y: 900, z: 0}
     });
+    window.onload = function() {
+      document.getElementById("tah_audio").play();
+  }
